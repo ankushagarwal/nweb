50da52b8c639ff6c16597e26bf46ef683e378cfb
Felipe Gorostiaga <feligorostiaga@gmail.com>
2021-02-14 17:19:23 -0300

    Ico mime type

--- nweb23.c
+++ nweb23.c
@@ -28,7 +28,7 @@ struct {
   {"jpg", "image/jpg" },
   {"jpeg","image/jpeg"},
   {"png", "image/png" },
-  {"ico", "image/ico" },
+  {"ico", "image/vnd.microsoft.icon" },
   {"zip", "image/zip" },
   {"gz",  "image/gz"  },
   {"tar", "image/tar" },
